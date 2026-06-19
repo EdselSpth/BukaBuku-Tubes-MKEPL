482a992b536302f880ee58dcec0a24ccd9fae769
tifunnyy <funnyrnd92@gmail.com>
2026-06-19 23:54:36 +0700

    [FNY] test CT berhasil

--- src/test/java/com/mycompany/account/AccountTest.java
+++ src/test/java/com/mycompany/account/AccountTest.java
@@ -14,7 +14,7 @@ public class AccountTest {
     @Test
     public void testGetPassword() {
         Account account = new Account("User123", "Password123");
-        assertEquals("PasswordSalah", account.getPassword());
+        assertEquals("Password123", account.getPassword());
     }
 
     @Test
